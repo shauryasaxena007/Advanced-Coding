dfb645f2c75ae17faa5b4cf31154843538ef6ea2
Shaurya Saxena <shauryasaxena147@gmail.com>
2026-08-03 18:32:35 +0530

    problem

--- problem_08.cpp
+++ problem_08.cpp
@@ -5,11 +5,12 @@ int main(){
     double distance, time;
     cout<<"Enter the distance in km :"<<endl;
     cin>>distance;
-    cout<<"Enter the time in hours :"<<endl;
-    cin>>time;
+    cout<<"Enter the time in seconds :"<<endl;
+    cin>>time ;
 
-    double speed = (distance/time) *3.6; // converting km/h to m/s
-    cout<<"The speed is : "<<speed<<" m/s"<<endl;
+    double speeds = (distance/time) *3.6; // converting km/h to m/s
+    cout<<"The speed is in m/s : "<<speeds<<" m/s"<<endl;
+
     return 0;
     
 }
