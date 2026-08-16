620a88c21cc095aae4c63aa7a282c900453622d6
Shaurya Saxena <shauryasaxena147@gmail.com>
2026-08-16 23:52:46 +0530

    problem19

---
+++ Problem_19.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cout << "Enter a number: ";
+    cin >> n;
+
+    int square = n * n;
+    int temp = n;
+
+    bool isAutomorphic = true;
+
+    while (temp > 0) {
+        int tp = temp % 10;
+        int sq = square % 10;
+        if (tp!= sq) {
+            isAutomorphic = false;
+            break;
+        }
+
+        temp = temp/10;
+        square = square/10;
+    }
+
+    if (isAutomorphic)
+        cout << "Automorphic Number";
+    else
+        cout << "Not Automorphic Number";
+
+    return 0;
+}
