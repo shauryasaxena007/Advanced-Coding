9ab18efed3f2450543451660d55f3dc81c91bd9e
Shaurya Saxena <shauryasaxena147@gmail.com>
2026-08-19 23:32:40 +0530

    prob23

---
+++ Problem_23.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<<"enter a no. wher you want to reach in fibonacci:";
+    cin>>n;
+
+    int a =0, b=1, c;
+    for(int i =1; i<=n; i++){
+        cout<<a<<" ";
+        c = a + b;
+        a = b;
+        b = c;
+    }
+    return 0;
+}
