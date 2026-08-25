0dd95eb4e3178bf675da1dbcada60097710a37be
Shaurya Saxena <shauryasaxena147@gmail.com>
2026-08-25 15:56:30 +0530

    hourglass

---
+++ Problem_30.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    // Upper half
+    for (int i = n; i >= 1; i--) {
+
+        // Spaces
+        for (int j = 1; j <= n - i; j++) {
+            cout << " ";
+        }
+
+        // Stars
+        for (int j = 1; j <= 2 * i - 1; j++) {
+            cout << "*";
+        }
+
+        cout << endl;
+    }
+
+    // Lower half
+    for (int i = 2; i <= n; i++) {
+
+        // Spaces
+        for (int j = 1; j <= n - i; j++) {
+            cout << " ";
+        }
+
+        // Stars
+        for (int j = 1; j <= 2 * i - 1; j++) {
+            cout << "*";
+        }
+
+        cout << endl;
+    }
+
+    return 0;
+}
