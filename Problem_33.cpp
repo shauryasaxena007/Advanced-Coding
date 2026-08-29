7e38e5f418e7a515a95cf53bf2828e56fb0030df
Shaurya Saxena <shauryasaxena147@gmail.com>
2026-08-29 23:24:22 +0530

    33

---
+++ Problem_33.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cout << "Enter number of rows: ";
+    cin >> n;
+
+    for (int i = 1; i <= n; i++) {
+
+        // Spaces
+        for (int j = 1; j <= n - i; j++) {
+            cout << " ";
+        }
+
+        // Numbers
+        for (int j = 1; j <= i; j++) {
+            cout << i << " ";
+        }
+
+        cout << endl;
+    }
+
+    return 0;
+}
