cb2bb1c41732a7d54c1a0aface19bddaaa19defc
Shaurya Saxena <shauryasaxena147@gmail.com>
2026-08-31 23:46:20 +0530

    pat

---
+++ Problem_34.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    for (int i = 1; i <= n; i++) {
+
+        // Spaces
+        for (int j = 1; j <= n - i; j++) {
+            cout << " ";
+        }
+
+        // Increasing alphabets
+        for (int j = 1; j <= i; j++) {
+            cout << char('A' + j - 1);
+        }
+
+        // Decreasing alphabets
+        for (int j = i - 1; j >= 1; j--) {
+            cout << char('A' + j - 1);
+        }
+
+        cout << endl;
+    }
+
+    return 0;
+}
