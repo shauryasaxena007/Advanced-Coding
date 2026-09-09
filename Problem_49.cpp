2b762ac61ffa6e4fc73a33bfee32d3ee0411214b
Shaurya Saxena <shauryasaxena147@gmail.com>
2026-09-09 23:30:34 +0530

    49

---
+++ Problem_49.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int arr[] = {1, 2, 3, 4, 5, 6};
+    int n = 6;
+
+    int even = 0;
+    int odd = 0;
+
+    for (int i = 0; i < n; i++) {
+
+        if (arr[i] % 2 == 0) {
+            even++;
+        }
+        else {
+            odd++;
+        }
+    }
+
+    cout << "Even = " << even << endl;
+    cout << "Odd = " << odd << endl;
+
+    return 0;
+}
