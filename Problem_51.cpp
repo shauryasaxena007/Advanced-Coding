4ed33c701c29cd9d9cd29ac30cf46dcbe20841a6
Shaurya Saxena <shauryasaxena147@gmail.com>
2026-09-10 15:22:49 +0530

    51

---
+++ Problem_51.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <climits>
+#include <cstdlib>
+using namespace std;
+
+int main() {
+    int arr[] = {10, 5, 20, 8};
+    int n = 4;
+
+    int minDiff = INT_MAX;
+
+    for (int i = 0; i < n; i++) {
+        for (int j = i + 1; j < n; j++) {
+
+            int diff = abs(arr[i] - arr[j]);
+
+            if (diff < minDiff)
+                minDiff = diff;
+        }
+    }
+
+    cout << "Smallest Difference = " << minDiff;
+
+    return 0;
+}
