e9f0658bae08b5fdb518199e279cdbfcd0a6eb86
Shaurya Saxena <shauryasaxena147@gmail.com>
2026-09-14 23:12:34 +0530

    54

---
+++ Problem_54.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int arr[] = {16, 17, 4, 3, 5, 2};
+    int n = 6;
+
+    int maxRight = arr[n-1];
+    cout << "Max Right Element = " << maxRight;
+    for(int i = n-2; i >= 0; i--){
+        if(arr[i] > maxRight){
+            cout<<" " << arr[i];
+            maxRight = arr[i];
+        }
+    }
+    return 0;
+}
